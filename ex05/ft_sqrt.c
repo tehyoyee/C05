ecf4e7f7f5374aa06998ebf523bf28d694977c72
taehykim <tehyoyee@gmail.com>
2021-09-24 22:49:55 +0900

    21/09/24 // v1.20

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -3,16 +3,17 @@
 
 int ft_sqrt(int nb)
 {
-    int i;
+    long long   i;
+    long long   nb_n;
 
     i = 0;
-
-    if (nb <= 0)
+    nb_n = nb;
+    if (nb_n <= 0)
         return (0);
-    while(i * i <= nb)
+    while(i * i <= nb_n)
         i++;
     i--;
-    if (i * i == nb)
+    if (i * i == nb_n)
         return (i);
     else
         return (0);
@@ -22,5 +23,5 @@ int main(void)
 {
     for (int i = 0; i < 20; i++)
         printf("%d : %d\n", i, ft_sqrt(i));
-    printf("%d : %d\n", 100000, ft_sqrt(2151253432));
+    printf("%d : %d\n", 100000, ft_sqrt(1600000000));
 }
